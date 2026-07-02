1aac770a36a4b07f3f3367a92e866e33b4e83c83
Coderado <102342706+Pyjavascript@users.noreply.github.com>
2026-07-02 17:14:47 +0530

    Change HTTP to HTTPS for manascience.in

--- app/main.py
+++ app/main.py
@@ -269,7 +269,7 @@ origins = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173",
     "http://192.168.29.34:5173",
-    "http://manascience.in",
+    "https://manascience.in",
     "https://www.manascience.in",
     "https://manascience.webflow.io"
 ]
